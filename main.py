774b738bc53668e03b12e0a50d6d8381c35381b5
procervantes <zoomleon@gmail.com>
2025-08-05 16:20:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,6 @@ def whatsapp_reply():
     msg.body(answer)
     return str(resp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
