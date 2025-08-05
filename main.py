c27b209482ebbbf04d9f54691f09d91c4ce131e4
procervantes <zoomleon@gmail.com>
2025-08-05 10:54:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Servidor Flask corriendo correctamente"
+def home():
+    return '¡Hola desde Render!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
